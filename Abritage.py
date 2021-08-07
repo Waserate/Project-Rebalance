21fa918900d35b25ae6ee4d7479b52239f5ece68
Waserate <tanatorn.vask@gmail.com>
2021-08-07 13:46:47 +0700

    ทำไฟล์ Config

--- Abritage.py
+++ Abritage.py
@@ -16,15 +16,18 @@ import json
 import requests
 from time import time, sleep
 from datetime import datetime
+import configparser
 
 #ส่วนของ Line Token
-token = 'PWCzRFGjpkxtUJ9JUzjq5wHIWsJ8sRkywCCkDA0suCd'  #ที่ต้อง Config
-messenger = Sendline(token)
+# token = ''  #ที่ต้อง Config
+# token = 'PWCzRFGjpkxtUJ9JUzjq5wHIWsJ8sRkywCCkDA0suCd'  #ที่ต้อง Config
 
 # API ของ Biannce 
-api_key = '9PkHxLgQ3lGyHIeUi6tw1HsQrC6nBnHigw99WY7gfZAf597jaMxKMXPI58col6FC'  #ที่ต้อง Config
-api_secret = 'QxKdJLR1GIRRisjEb9Wn2j7McCZ8zgMCLz4rwbMAkPM2UqsOFKiEnijtxvKeGPXt'  #ที่ต้อง Config ตั้งให้เป็น Read เท่านั้น 
-client = Client(api_key, api_secret)
+# api_key = ''  #ที่ต้อง Config
+# api_secret = ''  #ที่ต้อง Config ตั้งให้เป็น Read เท่านั้น 
+# api_key = '9PkHxLgQ3lGyHIeUi6tw1HsQrC6nBnHigw99WY7gfZAf597jaMxKMXPI58col6FC'  #ที่ต้อง Config
+# api_secret = 'QxKdJLR1GIRRisjEb9Wn2j7McCZ8zgMCLz4rwbMAkPM2UqsOFKiEnijtxvKeGPXt'  #ที่ต้อง Config ตั้งให้เป็น Read เท่านั้น 
+# client = Client(api_key, api_secret)
 c = CurrencyRates()
 
 # API ของ Bitkub 
@@ -32,8 +35,15 @@ API_HOST = 'https://api.bitkub.com'
 API_KEY = '' #ไม่จำเป็นต้องใส่ก็ Get ค่าออกมาได้ 
 API_SECRET = b'' #ไม่จำเป็นต้องใส่ก็ Get ค่าออกมาได้ 
 
-#ปรับเวลา
-TIME= 10 
+def read_config(file_path):
+    config = configparser.ConfigParser()
+    config.read(file_path)
+    for section in config.sections():
+        for key in config[section]:
+            global messenger, client, TIME
+            messenger = Sendline(config[section]['token_line'])
+            client = Client(config[section]['api_key_binance'], config[section]['api_secret_binance'])
+            TIME = int(config[section]['time_check'])
 
 #ส่วนที่ห้ามยุ่งของ Bitkub  
 def json_encode(data):
@@ -87,6 +97,8 @@ def ticker(coin = 'THB_BNB', variable = 'highestBid'): #หาราคาต่
    #  print (result)
     return result
 
+def roundDec(number):
+    return round(number,2)
 
 def calculate():
    print ("-----------------")
@@ -111,8 +123,8 @@ def calculate():
    result_percentage = (((bnb_bitkub_rate - biannce_thai_rate)/biannce_thai_rate)*100) #ได้ค่ามาไว้ในตัวแปร 
 #    print ("Diff (%):",result_percentage) #ปริ้นออกมาใน Terminal  
 #    print ('Rate BNB :',price_bnb,'USD\nRate BNB (Binance) :',biannce_thai_rate,'Bath\nRate BNB (Bitkub) :',bnb_bitkub_rate,'Bath\nDiff (Bath):',result_bath,'\nDiff (%):',result_percentage)
-   print ('Rate BNB : %f USD\nRate BNB (Binance) : %f Bath\nRate BNB (Bitkub) : %f Bath\nDiff (Bath): %f Bath\nDiff (per.): %f '%(price_bnb,biannce_thai_rate,bnb_bitkub_rate,result_bath,result_percentage))
-   messenger.sendtext('\nRate BNB : %f USD\nRate BNB (Binance) : %f Bath\nRate BNB (Bitkub) : %f Bath\nDiff (Bath) : %f \nDiff (per.) : %f '%(price_bnb,biannce_thai_rate,bnb_bitkub_rate,result_bath,result_percentage))
+   print ('Rate BNB : %.2f USD\nRate BNB (Binance) : %.2f Baht\nRate BNB (Bitkub) : %.2f Baht\nDiff (Baht): %.2f Baht\nDiff (per.): %.2f %s'%(roundDec(price_bnb),roundDec(biannce_thai_rate),roundDec(bnb_bitkub_rate),roundDec(result_bath),roundDec(result_percentage), "%"))
+   messenger.sendtext('\nRate BNB : %.2f USD\nRate BNB (Binance) : %.2f Baht\nRate BNB (Bitkub) : %.2f Baht\nDiff (Baht): %.2f Baht\nDiff (per.): %.2f %s'%(roundDec(price_bnb),roundDec(biannce_thai_rate),roundDec(bnb_bitkub_rate),roundDec(result_bath),roundDec(result_percentage), "%"))
 
 #    if result_percentage >= 5:
 #         messenger.sendtext('เกิน 5 เปอร์เซ็นต์ สามารถทำ Abritage ได้!!')
@@ -123,6 +135,8 @@ def calculate():
 #    elif result_percentage <= 0.99:
 #         messenger.sendtext('ยังไม่มีโอกาส')
 
+# Read ค่า config
+read_config("./config.ini")
 timer(TIME)
 
 
